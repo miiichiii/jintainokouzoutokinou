3aff1015d7da3f67acc626dcaff7ccc39836d638
miiichiii <60574612+miiichiii@users.noreply.github.com>
2026-04-10 00:31:27 +0900

    Set production LIFF ID

--- quiz-auth-config.js
+++ quiz-auth-config.js
@@ -1,6 +1,6 @@
 window.QUIZ_RUNTIME_CONFIG = Object.assign(
   {
-    liffId: "",
+    liffId: "2007823353-3UugkNL5",
     apiBaseUrl: "https://line-anatomy-bot.onrender.com",
   },
   window.QUIZ_RUNTIME_CONFIG || {}
